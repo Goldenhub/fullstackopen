248624bd0ad7809fc1418214e084c6aae45cdcb5
goldenhub <goldenazubuike@gmail.com>
2022-06-25 09:47:52 +0100

    phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,45 +1,72 @@
-import './App.css';
-import {useState} from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: "Arto Hellas", number: "22334455" },
+  ]);
+  const [newUser, setNewUser] = useState({
+    name: "",
+    number: "",
+  });
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
+
     // console.log(e.target)
     let stringedPersons = JSON.stringify(persons);
-    let check = stringedPersons.includes(JSON.stringify({name: newName}));
-    if(!check){
-      setPersons(persons.concat({name: newName}))
-      setNewName('');
-    }else{
-      alert(`${newName} is already added to phonebook`)
+    let checkName = stringedPersons.includes(JSON.stringify(newUser.name.trim()));
+    let checkNumber = stringedPersons.includes(JSON.stringify(newUser.number.trim()));
+    if (!checkName && !checkNumber) {
+      setPersons(persons.concat(newUser));
+      setNewUser((prev) => ({
+        ...prev,
+        name: "",
+        number: "",
+      }));
+    } else {
+      if(checkName && checkNumber){
+        alert(`${newUser.name.trim()} details is already added to phonebook`);
+      }else if(checkNumber){
+        alert(`${newUser.number.trim()} is already added to phonebook`);
+      }else{
+        alert(`${newUser.name.trim()} is already added to phonebook`);
+      }
     }
   }
 
-  function handleChange(e){
-    setNewName(e.target.value)
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setNewUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   }
-
   return (
     <div>
-      <div>debug: {newName}</div>
+      <div>debug: {JSON.stringify(newUser)}</div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name:{" "}
+          <input value={newUser.name} name="name" onChange={handleChange} />
+        </div>
+        <div>
+          number:{" "}
+          <input value={newUser.number} name="number" onChange={handleChange} />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) => (
+        <p key={person.name}>
+          {person.name}: {person.number}
+        </p>
+      ))}
     </div>
-  )
+  );
 }
 
 export default App;
