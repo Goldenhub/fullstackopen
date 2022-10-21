ab5fe802ce3bb8bbfc0f1f9418a666e706cab0ae
Golden <goldenazubuike@gmail.com>
2022-10-21 02:39:40 +0100

    connected to backend and production build

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 let notes = [
     {
@@ -66,6 +67,9 @@ let notes = [
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('build'))
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
@@ -120,7 +124,7 @@ app.post('/api/notes', (request, response) => {
 })
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server started running on PORT ${PORT}` );
 });
