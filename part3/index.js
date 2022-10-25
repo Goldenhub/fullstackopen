b83cdb6a3aa886c8367688107ecf6e87c8f7fefd
Golden <goldenazubuike@gmail.com>
2022-10-25 16:45:26 +0100

    added eslint for static analysis

--- part3/index.js
+++ part3/index.js
@@ -1,14 +1,17 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const express = require('express')
 require ('dotenv').config()
-const app = express();
+const app = express()
 const cors = require('cors')
 
 const Note = require('./models/note')
 
 
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 app.use(express.static('build'))
 
 
@@ -18,22 +21,22 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    response.json({status: 200, message: "successfully", data: notes})
+    response.json({ status: 200, message: 'successfully', data: notes })
   })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-  const id = request.params.id;
-  
+  const id = request.params.id
+
   Note.findById(id)
     .then(note => {
       if(note)
-        response.json({ status: 200, message: "Successful", data: note });
+        response.json({ status: 200, message: 'Successful', data: note })
       else{
-        response.statusMessage = "Resource Not Found";
-        response.status(404).json({ status: 404, message: "Uh oh! Can't find something"}); 
+        response.statusMessage = 'Resource Not Found'
+        response.status(404).json({ status: 404, message: 'Uh oh! Can\'t find something' })
       }
-  })
+    })
     .catch(err => next(err))
 
 })
@@ -41,52 +44,49 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 
 app.delete('/api/notes/:id', (request, response, next) => {
-  const id = request.params.id;
+  const id = request.params.id
   Note.findByIdAndRemove(id)
     .then(note => {
       console.log(note)
       if (note) {
-        response.statusMessage = "Note Deleted";
-        response.status(204).end();
+        response.statusMessage = 'Note Deleted'
+        response.status(204).end()
       }
       else {
-        response.statusMessage = "Resource Not Found";
-        response.status(404).end();
+        response.statusMessage = 'Resource Not Found'
+        response.status(404).end()
       }
     })
     .catch(err => next(err))
 })
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({
-      "error": "content missing"
-     })
-  }
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   const note = new Note({
     content: body.content,
     important: body.important,
     date: new Date(),
   })
-  
-  note.save().then(savedNote => {
-    response.json({status: 200, message: "Note Added", data: savedNote});
-  })
+
+  note.save()
+    .then(savedNote => {
+      response.json({ status: 200, message: 'Note Added', data: savedNote })
+    })
+    .catch(err => next(err))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const id = request.params.id;
-    const body = request.body;
-    const note = {
-      content: body.content,
-      important: body.important
-    }
-    Note.findByIdAndUpdate(id, note, { new: true })
-      .then(updatedNote => {
-        response.json({ status: 204, message: "successfully updated", data: updatedNote });
-      })
+  const id = request.params.id
+  const { content, important } = request.body
+  Note.findByIdAndUpdate(
+    id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      response.json({ status: 204, message: 'successfully updated', data: updatedNote })
+    })
     .catch(err => next(err))
 })
 
@@ -98,10 +98,12 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message);
+  console.log(error.message)
 
   if (error.name === 'CastError') {
-    response.status(400).json({status: 400, error: 'malformatted ID'})
+    response.status(400).json({ status: 400, error: 'malformatted ID' })
+  } else if (error.name === 'ValidationError') {
+    response.status(400).json({ status: 400, error: error.message })
   }
 
   next(error)
@@ -109,7 +111,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server started running on PORT ${PORT}`);
+  console.log(`Server started running on PORT ${PORT}`)
 })
--- part3/models/note.js
+++ part3/models/note.js
@@ -1,3 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
@@ -5,25 +8,32 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(err => {
-        console.log('error connecting to MongoDB:', err.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(err => {
+    console.log('error connecting to MongoDB:', err.message)
+  })
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean
+  content: {
+    type: String,
+    required: true,
+    minLength: 5
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean
 })
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
